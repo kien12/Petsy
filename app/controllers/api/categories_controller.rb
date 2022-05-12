98065d7a1b1778bed67520bc74fcb6b75210f3ad
Kien Nguyen <nguyen_kien@live.com>
2022-05-11 20:14:34 -0700

    work on category index json

---
+++ app/controllers/api/categories_controller.rb
@@ -0,0 +1,20 @@
+class Api::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+
+    render 'api/categories/index'
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+    render 'api/categories/show'
+  end
+
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
+

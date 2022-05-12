0a6e4c9ba87f40284a25b027867bdc851d471b07
Kien Nguyen <nguyen_kien@live.com>
2022-05-12 16:05:29 -0700

    config webpack

--- frontend/petsy.jsx
+++ frontend/petsy.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
 
-  ReactDOM.render(<h1>naclemd is working<h1> </>, root)
+  ReactDOM.render(<h1>naclemd is working</h1>, root)
 })
 
 

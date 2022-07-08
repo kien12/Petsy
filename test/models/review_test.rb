02cffff5be1260a179b2d31704c8cc2b76f0cc5a
Kien Nguyen <nguyen_kien@live.com>
2022-07-08 15:53:03 -0700

    before migration of createUsers

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#  product_id :integer          not null
+#  rating     :integer          not null
+#
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase

311c0bcf309941e519fb704fe4255939f74863a8
Kien Nguyen <nguyen_kien@live.com>
2022-07-08 17:03:16 -0700

    Create user Model and Application Controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,11 +11,39 @@
 #
 class Review < ApplicationRecord
   validates :user_id, :product_id, :rating, presence: true
-    
-  belongs_to :users
-    foreign_key: :user_id
-    class_name: 'User'
-  belongs_to :product_id
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, length: { minimum: 6, allow_nil: true }
   
+  attr_reader :password;
+  after_initialize :ensure_session_token
+
+  belongs_to :users,
+    foreign_key: :user_id,
+    class_name: 'User'
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    (user && user.is_password?(password) ? user : nil)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+  
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
 
 end

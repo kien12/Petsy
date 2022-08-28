f15f3a05e82849d37a95e43bd34aca569a2bcc50
Kien Nguyen <nguyen_kien@live.com>
2022-08-28 14:46:28 -0700

    added routes to reviews [create, destroy, update, show]

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
   namespace :api, defaults: {format: :json} do
     resources :categories, only: [:index, :show]
-    resources :products, only: [:create, :index, :show, :patch, :destroy]
+    resources :products, only: [:create, :index, :show, :update, :destroy]
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
+    resources :reviews, only: [:create, :destroy, :update, :show]
   end
   
     

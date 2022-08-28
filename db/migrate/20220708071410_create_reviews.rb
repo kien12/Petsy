fcbdefefc15e7a0d123f07bce29b490e333dbd3d
Kien Nguyen <nguyen_kien@live.com>
2022-08-28 14:22:38 -0700

    before reviews migration

--- db/migrate/20220708071410_create_reviews.rb
+++ db/migrate/20220708071410_create_reviews.rb
@@ -1,8 +1,15 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-
+      t.text :body
+      t.integer :user_id, null: false
+      t.integer :product_id, null: false
+      t.integer :rating, null: false
       t.timestamps
     end
+    add_index :reviews, [:product_id, :name]
+    add_index :reviews, [:user_id, :name]
   end
+
 end
+

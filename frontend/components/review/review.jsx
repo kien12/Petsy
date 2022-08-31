79343783279833f97006cdf2e4f9cf5d5a5ffbdd
Kien Nguyen <nguyen_kien@live.com>
2022-08-30 23:13:47 -0700

    finish review component

---
+++ frontend/components/review/review.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Review extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+
+    return(
+      <div>REVIEW</div>
+    )
+  }
+}
+
+export default Review;

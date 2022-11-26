484e326a3bd4435334fe0c85aa2b679f4ade808e
Kien Nguyen <nguyen_kien@live.com>
2022-11-26 11:27:50 -0800

    create HeroBanner

---
+++ frontend/components/splash/hero_banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class HeroBanner extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    
+    return(
+      <div>
+        herobanner
+      </div>
+    )
+  }
+}

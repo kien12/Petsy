cea97b442e1195bd97edd906f70f7ffe4d6a3b45
Kien Nguyen <nguyen_kien@live.com>
2022-12-28 17:21:40 -0800

    add content to about jsx

--- frontend/components/splash/about.jsx
+++ frontend/components/splash/about.jsx
@@ -11,16 +11,28 @@ class About extends React.Component {
       <h1>What is Ketsy?</h1>
       <div className='about-block-container'>
         <div className='about-block'>
-          <h2 className='about-header'>header</h2>
-          <p className='about-details'>details</p>
+          <h2 className='about-header'>
+            header
+          </h2>
+          <p className='about-details'>
+            details
+          </p>
         </div>
         <div className='about-block'>
-          <h2 className='about-header'>header</h2>
-          <p className='about-details'>details</p>
+          <h2 className='about-header'>
+            header
+          </h2>
+          <p className='about-details'>
+            details
+          </p>
         </div>
         <div className='about-block'>
-          <h2 className='about-header'>header</h2>
-          <p className='about-details'>details</p>
+          <h2 className='about-header'>
+            header
+          </h2>
+          <p className='about-details'>
+            details
+          </p>
         </div>
       </div>
     </div>      

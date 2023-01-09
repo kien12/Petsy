205c202fbe115ec1e9d7b542cdc787dea0323e68
Kien Nguyen <nguyen_kien@live.com>
2023-01-08 19:33:17 -0800

    add seller_name to categorys show

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,7 +19,7 @@ class Product < ApplicationRecord
     foreign_key: :category_id,
     class_name: "Category"
 
-  belongs_to :seller,
+  belongs_to :sellers,
     foreign_key: :seller_id,
     class_name: "Seller"
 

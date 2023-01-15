890a17fd0d15449ccb2f9cf286d137e98288a750
Kien Nguyen <nguyen_kien@live.com>
2023-01-15 12:46:01 -0800

    created cart_item_params

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -1,2 +1,11 @@
 class Api::CartItemsController < ApplicationController
+  def create
+    @cart_item = Cart_item.new(cart_item_params)
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:user_id, :product_id, :quantity)
+  end
+
 end

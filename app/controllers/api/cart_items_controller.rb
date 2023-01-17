2b4a7ad5c8e9a053d696f149d2231babd79b6626
Kien Nguyen <nguyen_kien@live.com>
2023-01-16 23:52:04 -0800

    work on cart items controller

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -1,16 +1,13 @@
 class Api::CartItemsController < ApplicationController
   
   def index
-    if @cart_items = CartItem.all.select { |cart_item| cart_item.user_id == currentUser.id}
+    if @cart_items = CartItem.all.select { |cart_item| cart_item.user_id == current_user.id}
       render '/api/cart_items/index'
     else
       return nil
     end
   end
 
-  def create
-    @cart_item = CartItem.new(cart_item_params)
-  end
 
   private
   def cart_item_params

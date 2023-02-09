5cf0e7c95624d83d865270671dd43a364942f98d
Kien Nguyen <nguyen_kien@live.com>
2023-02-09 12:23:25 -0800

    add createCartItem to cart items container

--- frontend/components/cart_items/cart_items_container.jsx
+++ frontend/components/cart_items/cart_items_container.jsx
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import CartItems from './cart_items';
+import { createCartItem } from '../../actions/cart_items_actions'
 
 const mSTP = state => ({
   cartItems: Object.values(state.entities.cartItems)
 })
 
 const mDTP = dispatch => ({
-  
+  createCartItem: cartItem => dispatch(createCartItem(cartItem))
 })
 
-export default connect(mSTP, null)(CartItems)
+export default connect(mSTP, mDTP)(CartItems);

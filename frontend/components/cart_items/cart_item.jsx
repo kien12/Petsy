4f92d0b5e9e6a16e2c21eec02568d328f9bf6aec
Kien Nguyen <nguyen_kien@live.com>
2023-03-14 23:11:41 -0700

    add numberOfCartItems on cart button

--- frontend/components/cart_items/cart_item.jsx
+++ frontend/components/cart_items/cart_item.jsx
@@ -20,7 +20,7 @@ class CartItem extends React.Component {
           <select><option value="1">1</option></select>
           <div>{price}</div>
         </div>
-        <button>Removeeeeeeeeee</button>
+        <button>Removeeeeeeeeeee</button>
       </div>
     )
   }
